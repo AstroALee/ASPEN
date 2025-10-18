d9da63a2fa6a4b3c26ef71106d21fbc4d350cd2a
Aaron Lee <AstroALee@users.noreply.github.com>
2025-10-18 00:24:39 -0700

    Update simple_calculations.py
    tweaks

--- aspen/simple_calculations.py
+++ aspen/simple_calculations.py
@@ -11,7 +11,7 @@ def scientific_notation(input_value: float) -> tuple[float, int]:
 
     Args:
         input_value (float): The number to convert.
-    
+
     Returns:
         tuple[float, int]: The coefficient and exponent of the input value in scientific notation.
     """
@@ -21,7 +21,22 @@ def scientific_notation(input_value: float) -> tuple[float, int]:
     # Check for zero
     if input_value == 0:
         return 0.0, 0
-    exponent = int(np.floor(np.log10(abs(input_value))))
+    exponent =  np.log10(abs(input_value))//1 # float 
+    exponent = int(exponent) # int 
     coefficient = input_value / (10 ** exponent)
     return coefficient, exponent
 
+
+def round_uncertainty(pbest : float, dp: float) -> tuple[float, float]:
+    """
+    Rounds the uncertainty to one or two significant figures and rounds the best estimate to the same decimal place.
+    Args:
+        pbest (float): The best estimate.
+        dp (float): The uncertainty.
+    Returns:
+        tuple[float, float]: The rounded best estimate and uncertainty.
+    """
+    # A negative second input for round means rounding to the left of the decimal point.
+    round_to = -int(scientific_notation(dp)[1]) if (scientific_notation(dp)[0])//1>2 \
+        else 1-int(scientific_notation(dp)[1])
+    return round(pbest, round_to), round(dp, round_to)
